1b5efba597b8f8d7573cfdbb6da5c7cddb54126c
VincentMC <vincemcgaley@gmail.com>
2019-03-30 16:45:21 +0000

    Updated/added logos and worked on layout shell

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+var favicon = require('serve-favicon');
 //this statement is required to use static files
 
 const app = express();
 const port = 3000;
 //this tells the app to use files in the public folder
+app.use(favicon(__dirname + '/public/images/icon.ico')); 
 app.use(express.static('public'));
 app.use('/', require('./routes/routes'));
 app.set("view engine", "pug");
